1e4cc6e764c2f750e8e2f375f0a39f71084bb5bd
Prashant Yadav <prashant.yadav@terminus.com>
2024-03-17 14:46:48 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Fair Billing
+
+Fair Billing is a Python program designed to analyze log files containing user session data and calculate information based on the duration of user sessions.
+
+### Features
+
+- Log File Processing: Reads log files containing user session data.
+- Session Calculation: Calculates the duration of each user session.
+- Fair Billing: Generates fair billing information for each user based on their session durations.
+- Error Handling: Handles errors gracefully, including file not found and invalid log data.
+
+### Usage
+
+1.Installation:
+
+    - Clone this repository to your local machine or extract the zip file.
+    - Make sure you have Python 3 installed.
+
+2.Run the Program:
+
+- Navigate to the directory containing the Fair Billing program.
+- Run the program by executing the following command in your terminal:
+
+```shell
+  python fair_billing.py 'samplelog.txt'
+```
+
+- Replace log.txt with the path to your log file.
+
+  3.View Results:
+
+- The program will process the log file and print the fair billing information for each user to the console.
+
+### Input Format
+
+The input log file should contain lines in the following format:
+14:02:03 ALICE99 Start
+14:02:05 CHARLIE End
+14:02:34 ALICE99 End
+14:02:58 ALICE99 Start
+14:03:02 CHARLIE Start
+14:03:33 ALICE99 Start
+
+### Output Format
+
+USERNAME NUMBER_OF_SESSIONS TOTAL_SESSION_TIME
+USERNAME: Unique identifier for the user.
+NUMBER_OF_SESSIONS: The total number of sessions for the user.
+TOTAL_SESSION_TIME: The total duration of all sessions for the user in seconds.
+
+Sample Output:
+
+```python
+ALICE99 4 240
+CHARLIE 3 37
+```
---
+++ fair_billing.py
@@ -0,0 +1,141 @@
+import re
+import sys
+from datetime import datetime
+
+
+class LineInPieces:
+    """Class to represent the lines in pieces"""
+
+    def __init__(self):
+        self.hours = None
+        self.minutes = None
+        self.seconds = None
+        self.userid = None
+        self.action = None
+        self.valid = False
+
+
+class UserSession:
+    """Class to represent user session"""
+
+    def __init__(self, userid):
+        self.userid = userid
+        self.start_time = None
+        self.end_time = None
+
+
+class UserReport:
+    """Class to represent user report for eg user,number of sessions and total session time"""
+
+    def __init__(self, userid, number_of_sessions, total_session_time):
+        self.userid = userid
+        self.number_of_sessions = number_of_sessions
+        self.total_session_time = total_session_time
+
+
+class FairBilling:
+
+    def read_log_file_to_list(self, file_name):
+        try:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                return lines
+        except FileNotFoundError:
+            raise FileNotFoundError(f"No such file {file_name}")
+
+    def process_valid_lines(self, lines):
+        line_in_pieces_list = []
+        for line in lines:
+            line_in_pieces = self.filter_valid_data(line)
+            if line_in_pieces.valid and line_in_pieces.action in ['Start', 'End']:
+                line_in_pieces_list.append(line_in_pieces)
+            # else:
+            #     print(f"Invalid: Log data {line.strip()} - Skipped")
+        return line_in_pieces_list
+
+    def create_user_session_map(self, lines):
+        user_session_map = {}
+        for line in lines:
+            if line.userid not in user_session_map:
+                user_session_map[line.userid] = []
+            user_session_map[line.userid] = self.process_line(line, user_session_map[line.userid])
+        return user_session_map
+
+    def process_line(self, line, user_session_list):
+        try:
+            line_time = datetime.strptime(f"{line.hours}:{line.minutes}:{line.seconds}", '%H:%M:%S').time()
+        except BaseException:
+            line_time = None
+        if line.action == 'Start':
+            user_session = UserSession(line.userid)
+            user_session.start_time = line_time
+            user_session_list.append(user_session)
+        elif line.action == 'End':
+            for user_session in user_session_list:
+                if user_session.end_time is None:
+                    user_session.end_time = line_time
+                    break
+            else:
+                user_session = UserSession(line.userid)
+                user_session.end_time = line_time
+                user_session_list.append(user_session)
+        return user_session_list
+
+    def filter_valid_data(self, line):
+        line_in_pieces = LineInPieces()
+        pattern = re.compile(r'^([01]\d|2[0-3]):([0-5]\d):([0-5]\d) ([A-Z0-9]+) (Start|End)$')
+        match = pattern.match(line.strip())
+        if match:
+            line_in_pieces.hours, line_in_pieces.minutes, line_in_pieces.seconds, line_in_pieces.userid, line_in_pieces.action = match.groups()
+            # check for valid user name
+            user_name = line_in_pieces.userid
+            user_name_pattern = r'(.)\1{2,}'
+            if bool(re.search(user_name_pattern, user_name)):
+                line_in_pieces.valid = False
+            else:
+                line_in_pieces.valid = True
+        return line_in_pieces
+
+    def process_file_as_list(self, lines):
+        pieces_list = self.process_valid_lines(lines)
+        # checking for the first time in file
+        first_time_in_file = datetime.strptime(f"{pieces_list[0].hours}:{pieces_list[0].minutes}:{
+                                               pieces_list[0].seconds}", '%H:%M:%S').time()
+        # checking the last time in the file
+        last_time_in_file = datetime.strptime(
+            f"{pieces_list[-1].hours}:{pieces_list[-1].minutes}:{pieces_list[-1].seconds}", '%H:%M:%S').time()
+        user_session_map = self.create_user_session_map(pieces_list)
+        results = []
+        for user, sessions in user_session_map.items():
+            total = 0
+            for session in sessions:
+                if session.start_time is None:
+                    session.start_time = first_time_in_file
+                if session.end_time is None:
+                    session.end_time = last_time_in_file
+                total += (datetime.combine(datetime.min, session.end_time) -
+                          datetime.combine(datetime.min, session.start_time)).total_seconds()
+            results.append(UserReport(user, len(sessions), total))
+        return results
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) < 2:
+            print("Wrong number of arguments:", len(sys.argv) - 1)
+            print("Syntax is: python fair_billing.py <path to file>")
+            sys.exit(1)
+
+        file_name = sys.argv[-1]
+        print(file_name)
+        fair_billing = FairBilling()
+        print("=> Reading log file:", file_name)
+        lines = fair_billing.read_log_file_to_list(file_name)
+        print("=> Processing in progress")
+        results = fair_billing.process_file_as_list(lines)
+        print("******* User Reports *******")
+        for result in results:
+            print(f"{result.userid} {result.number_of_sessions} {result.total_session_time}")
+
+    except BaseException as e:
+        print("Something went wrong:", e)
